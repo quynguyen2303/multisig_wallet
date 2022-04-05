cdd7c22dfe2384619da7d9b9ab14b00564c9b9b6
Quy Nguyen <nguyenphuquy2303@gmail.com>
2022-04-05 20:45:50 +0700

    build web app

--- react_truffle_box/truffle-config.js
+++ react_truffle_box/truffle-config.js
@@ -1,5 +1,10 @@
 const path = require("path");
 const provider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+
+const secret = JSON.parse(
+  fs.readFileSync(".secrect.json").toString().trim()
+);
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,15 +14,12 @@ module.exports = {
     kovan: {
       provider: () =>
         new provider(
-          [
-            "5ec33ce4d60d6b6f8b0c47ff31d5a669129c0e2090d3d9d8078fd001a6413187",
-            "5609b0ddcaf8b12c0fc50a5cf12ef05c7d7ff4852c42b6545cb601df27c4983c",
-            "cd79187b55156795c4b1df543c6b2fa1075538961502d10d0cf94b5526dfb802"
-          ],
+          secret.privateKeys,
           "https://kovan.infura.io/v3/30c46791dc8445929d39d68688ce9c73",
           0, // start of first private key
           3 // number of address
-        )
+        ),
+      network_id: '*'
     }
     // develop: {
     //   port: 8545
