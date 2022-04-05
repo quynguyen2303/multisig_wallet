312fc71f981a52004395ca3bff03ccda34e31cdf
Quy Nguyen <nguyenphuquy2303@gmail.com>
2022-04-05 08:52:15 +0700

    Connect to Metamask

--- react_truffle_box/client/src/App.js
+++ react_truffle_box/client/src/App.js
@@ -16,7 +16,7 @@ function App() {
   // Initialization
   useEffect(() => {
     const init = async () => {
-      const web3 = getWeb3();
+      const web3 = await getWeb3();
       const accounts = await web3.eth.getAccounts();
       const wallet = await getWallet(web3);
       const approvers = await wallet.methods.getApprovers().call();
